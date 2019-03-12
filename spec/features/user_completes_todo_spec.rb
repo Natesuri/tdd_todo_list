8ede6c23eb9192f4ca43796cc59e2f6e2822b652
Natesuri <natesuri@gmail.com>
2019-03-12 10:42:44 -0400

    Tests for marking a todo as completed
    TDD/BDD following a thoughtbot tutorial
    
    A feature test that has a user sign_in, create a todo, then mark that
    todo as completed, then check to see if the page has
    a .todos li.completed.
    
    Also, a unit test to check the #completed? functionality.
    If a todo has a completed_at timestamp, is should "be_completed".
    If the timestamp is nil, then it should not "be_completed."

---
+++ spec/features/user_completes_todo_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+# Capybara syntax for designating feature test
+feature 'user completes todo' do
+  # what the expecting passing behavior should be
+  scenario 'successfully' do
+    # tells Capybara to visit index.html
+    sign_in
+
+    #  to click on a link or button with the text 'Add a new todo'
+    click_on 'Add a new todo'
+    #  to fill in a form with a text field :title and the text to fill it with
+    fill_in 'Title', with: 'Create Rails TDD proof of concept'
+    #  to click on a link or button with the text 'Submit'
+    click_on 'Submit'
+
+    click_on 'Done'
+
+    # Sets a passing expectation, for there to be a list item containing the text
+    # within an element with the class '.todos'
+    expect(page).to have_css '.todos li.completed',
+                             text: 'Create Rails TDD proof of concept'
+  end
+end
